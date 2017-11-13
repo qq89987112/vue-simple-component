a957fea2c5b1ea3b262a86049be587144eb24c93
蔡荣庚 <89987112@qq.com>
2017-11-13 11:01:22 +0800

    v0.1

--- readme.md
+++ readme.md
@@ -1,3 +1,16 @@
 # simple components
+  
+# 一些技巧
+## 移动端empty界面
+    使用 empty选择器做页面无数据时展示的内容(before的背景图以及 content文字)
 
-## 需要有router,方便链接跳转
+## 汉字字体
+    使用 font-spider 做汉字字体
+    
+## 触底加载
+    scrollHeight === scrollTop + offsetHeight 做触底加载
+
+## 多行省略
+    scrollHeight>offsetHeight + css max-height 做多行省略
+  
+  
---
+++ src/components/plugins/validator/index.js
@@ -0,0 +1,41 @@
+export default {
+  install(Vue) {
+    Vue.prototype.$validate = function (options) {
+      // 正则代码 摘自本人另一个开源代码  https://github.com/qq89987112/RegExtractor
+      let
+        regExp = /\/(.+)\/([^\|]+)?(?:\|(.+))?/,
+        regStr,
+        result,
+        flags,
+        name,
+        key,
+        reg,
+        errorMsg;
+
+      for (key in options) {
+        if (!options.hasOwnProperty(key)) return;
+        result = regExp.exec(key);
+        regStr = result[1];
+        flags = result[2];
+        name = result[3];
+
+        if (regStr) {
+          reg = new RegExp(regStr, flags);
+          errorMsg = options[key];
+          result = reg.test(this[name]);
+          // 有错就返回,不把所有的错列出来
+          if(!result){
+            // 与后台格式保持一致
+            return Promise.reject(
+              {
+                "msg":errorMsg
+              }
+            );
+          }
+        }
+      }
+
+      return Promise.resolve();
+    }
+  }
+}
