5dbc6072acb0af499634df10a5ed896d41b46122
蔡荣庚 <89987112@qq.com>
2017-11-16 18:18:14 +0800

    v0.1

---
+++ src/components/simple-component/DrapDrop.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+
+    </div>
+</template>
+<script>
+    export default {
+        name: 'register-page',
+        data() {
+            return {}
+        },
+        methods: {},
+        created() {
+
+        }
+    }
+</script>
+<style lang="scss" rel="stylesheet/scss" scoped>
+
+</style>
