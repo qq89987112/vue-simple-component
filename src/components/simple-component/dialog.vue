fa6c508bfd592aa04e5f97c9c82f92900fd47ae0
蔡荣庚 <89987112@qq.com>
2017-11-21 14:24:21 +0800

    修改了dialog

--- src/components/simple-component/dialog.vue
+++ src/components/simple-component/dialog.vue
@@ -1,66 +1,76 @@
 <template>
   <div class="dialog-component" v-show="isShow">
     <section class="content" @click.prevent.stop="">
-      <slot name="content"></slot>
+      <slot name="content" :flag="flag"></slot>
     </section>
   </div>
 </template>
 <script>
-    export default {
-        name: 'm-dialog',
-        data() {
-            return {
-              isShow:false
-            }
-        },
-        methods: {
-          showDialog(){
-            setTimeout(()=>{
-              this.isShow = true;
-            },0)
-          },
-          closeDialog(){
-            this.isShow = false;
-          }
-        },
-        created(){
-          let self = this;
-          this.__outclick__ = function () {
-            if(self.isShow){
-              self.closeDialog();
-            }
-          };
-          window.addEventListener("click", this.__outclick__)
-        },
-        destroyed(){
-
-        },
-        mounted() {
-          let $el = this.$el;
-          let computedStyle = getComputedStyle($el.parentElement);
-          $el.style.width = computedStyle.width ;
-          $el.style.height = computedStyle.height ;
+  export default {
+    name: 'm-dialog',
+    data() {
+      return {
+        isShow: false,
+        flag: undefined
+      }
+    },
+    methods: {
+      showDialog() {
+        setTimeout(() => {
+          this.isShow = true;
+        }, 0)
+      },
+      closeDialog() {
+        this.isShow = false;
+      },
+      showDialogForResult() {
+        this.showDialog();
+        return new Promise((resolve, rejct) => {
+          this.$once("result", (data) => {
+            resolve(data);
+          })
+        });
+      },
+      setFlag(flag) {
+        this.flag = flag;
+      }
+    },
+    created() {
+      let self = this;
+      this.__outclick__ = function () {
+        if (self.isShow) {
+          self.closeDialog();
+        }
+      };
+      window.addEventListener("click", this.__outclick__)
+    },
+    destroyed() {
+      window.removeEventListener("click", this.__outclick__)
+    },
+    mounted() {
+      let $el = this.$el;
+      let computedStyle = getComputedStyle($el.parentElement);
+      $el.style.width = computedStyle.width;
+      $el.style.height = computedStyle.height;
 
 //          let context = this.content = this.$slots.content[0].context;
 //          context.$on('showDialog',(ref)=>{
 //            this.closeDialog();
 //          })
-        }
     }
+  }
 </script>
 <style lang="scss" rel="stylesheet/scss" scoped>
-  .dialog-component{
-    box-shadow: 0 0 0 9999px rgba(0,0,0,0.5);
+  .dialog-component {
+    box-shadow: 0 0 0 9999px rgba(0, 0, 0, 0.5);
     position: relative;
     z-index: 999;
-    background-color: rgba(0,0,0,0.5);
-    .content{
+    background-color: rgba(0, 0, 0, 0.5);
+    .content {
       position: absolute;
       top: 50%;
       left: 50%;
-      transform: translate(-50%,-50%);
-      width: 200px;
-      line-height: 200px;
+      transform: translate(-50%, -50%);
       background-color: #fff;
     }
   }
