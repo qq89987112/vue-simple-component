da41fd45eb10b1cc3c14ec734e7438bd481d26bd
蔡荣庚 <89987112@qq.com>
2017-12-06 17:40:06 +0800

    add load-more

--- src/js/utils/ui/load-more.js
+++ src/js/utils/ui/load-more.js
@@ -9,23 +9,34 @@
 
 export default function (element, options) {
   let
+    //为了不把一些杂七杂八的变量放在vue文件中
     env = options.env || {},
-    {page = 1, rows = 10} = options,
+    //默认已经读取了首屏,从第二页开始
+    {page = 2, rows = 10} = options,
     onLoad = (page, row, env) => {
-
     };
+    //不需要,在onLoad里自己根据env变量判断。
+    // willLoad = (page,row,env)=>{};
 
+    element.addEventListener("scroll",function (e) {
+      let srcElement = e.srcElement;
+      if(srcElement.scrollHeight === srcElement.scrollTop + srcElement.offsetHeight){
+        onLoad(page++,rows,env);
+      }
+    });
 
   return {
     onLoad(cb) {
       if (typeof cb === 'function') {
         onLoad = cb;
       }
+      return this;
     },
     reset() {
-      page = options.page || 1;
+      page = options.page || 2;
       rows = options.rows || 10;
       env = options.env || {};
+      return this;
     }
   }
 }
