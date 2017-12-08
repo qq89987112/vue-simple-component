a1573dfea3b3f06e8143c85c2d6b41c9c3c74264
蔡荣庚 <89987112@qq.com>
2017-12-08 17:48:38 +0800

    update caesium

--- src/views/calendar.vue
+++ src/views/calendar.vue
@@ -1,41 +1,41 @@
 <template>
-    <div class="calendar-page">
-      <button @click="open">打开</button>
-      <m-dialog ref="dialog">
-        <m-calendar @result="onResult"></m-calendar>
-      </m-dialog>
-    </div>
+  <div class="calendar-page">
+    <button @click="open">打开</button>
+    <m-dialog ref="dialog">
+      <m-calendar @result="onResult"></m-calendar>
+    </m-dialog>
+  </div>
 </template>
 <script>
-    import MDialog from "../components/simple-component/dialog.vue";
-    import MCalendar from "../components/simple-component/calendar/calendar.vue";
+  import MDialog from "../components/simple-component/dialog.vue";
+  import MCalendar from "../components/simple-component/calendar/calendar.vue";
 
-    export default {
-      components: {
-        MCalendar,
-        MDialog},
-      name: 'register-page',
-        data() {
-            return {
-            }
-        },
-        methods: {
-          onResult(result){
-            this.$refs.dialog.$emit("result",result);
-          },
-          open(){
-            this.$refs.dialog.showDialogForResult().then((data)=>{
-              this.$toast(data);
-            })
-          }
-        },
-        created() {
+  export default {
+    components: {
+      MCalendar,
+      MDialog
+    },
+    name: 'register-page',
+    data() {
+      return {}
+    },
+    methods: {
+      onResult(result) {
+        this.$refs.dialog.$emit("result", result);
+      },
+      open() {
+        this.$refs.dialog.showDialogForResult().then((data) => {
+          this.$toast(data);
+        })
+      }
+    },
+    created() {
 
-        }
     }
+  }
 </script>
 <style lang="scss" rel="stylesheet/scss" scoped>
-.calendar-page{
+  .calendar-page {
 
-}
+  }
 </style>
