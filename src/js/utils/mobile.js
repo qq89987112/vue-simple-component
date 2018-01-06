4101e621cb2fd22151fafc25a273a682d70b324b
蔡荣庚 <89987112@qq.com>
2018-01-06 15:17:14 +0800

    add mobile

--- src/js/utils/mobile.js
+++ src/js/utils/mobile.js
@@ -1,13 +1,25 @@
+import detector from "detector"
+
 const handle = function () {
   let activeElement = document.activeElement;
   activeElement.scrollIntoView();
   activeElement.scrollIntoViewIfNeeded();
 }
-export default {
+export const autoScorllIntoView  = {
   register(){
     window.addEventListener('resize',handle)
   },
   destory(){
     window.removeEventListener('resize',handle)
+  }
+}
+
+//解决IOS下需要点击两次
+export const clickFileInput  = (element)=>{
+  if(detector.os.name==='ios'){
+    element.click();
+    element.click();
+  }else{
+    element.click();
   }
 }
