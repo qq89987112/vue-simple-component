981ba3c7da97078a1091b345ae101592b86f064a
蔡荣庚 <89987112@qq.com>
2018-03-06 14:41:29 +0800

    update

--- src/components/route-side-menu/route-side-menu-item.vue
+++ src/components/route-side-menu/route-side-menu-item.vue
@@ -1,63 +1,56 @@
 <template>
-  <div class="side-menu-item"><slot/></div>
+  <div class="side-menu-item" :class="[active&&'active']" @click="onItemClick" ><slot/></div>
 </template>
 
 <script>
 
   export default {
-    name: 'route-side-menu',
+    name: 'route-side-menu-item',
+    props:["path"],
     computed: {
     },
     data() {
       return {
+        active:false
       }
     },
     components: {
     },
     created(){
+      this.active = this.$router.currentRoute.fullPath === this.path;
+      let func = this.routeHook = (to, form)=>{
+        this.active = to.fullPath === this.path;
+      };
+      this.$router.afterHooks.push(func)
+    },
+    beforeDestroy(){
+      let afterHooks = this.$router.afterHooks;
+      afterHooks.splice(afterHooks.indexOf(this.routeHook),1)
     },
     methods: {
+      onItemClick(){
+        let path = this.path;
+        path&&this.$router.push(path);
+      }
     }
   }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style lang="scss" rel="stylesheet/scss">
-  .home-page{
-    display: flex;
-    height: 100vh;
-      .side-menu{
-        display: flex;
-        flex-direction: column;
-        background-color: #314659;
-        color: white;
-        .side-menu-logo{
+<style lang="scss" rel="stylesheet/scss" scoped>
+  .side-menu-item {
+    cursor: pointer;
+    width: 100px;
+    text-align: center;
+    line-height: 3em;
+    color: rgba(255, 255, 255, 0.65);
+    .iconfont {
+      margin-right: 10px;
+    }
 
-        }
-        .side-menu-content{
-          flex: 1;
-          .side-menu-item{
-            @extend .side-menu-bottom;
-          }
-        }
-
-        .side-menu-bottom{
-          cursor: pointer;
-          width: 100px;
-          text-align: center;
-          line-height: 3em;
-          .iconfont{
-            margin-right: 10px;
-          }
-
-          &.active{
-            background-color: #fff;
-            color: #314659;
-          }
-        }
-      }
-
+    &.active {
+      background-color: #1890ff;
+      color: white;
+    }
   }
-
-
 </style>
--- src/components/route-side-menu/route-side-menu.vue
+++ src/components/route-side-menu/route-side-menu.vue
@@ -6,7 +6,6 @@
     <div class="side-menu-content">
       <slot></slot>
     </div>
-    <div class="side-menu-bottom"><slot name="bottom"/></div>
   </div>
 </template>
 
@@ -37,7 +36,7 @@
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style lang="scss" rel="stylesheet/scss">
+<style lang="scss" rel="stylesheet/scss" scoped>
   .route-side-menu-component {
     display: flex;
     flex-direction: column;
@@ -48,26 +47,6 @@
     }
     .side-menu-content {
       flex: 1;
-      .side-menu-item {
-        @extend .side-menu-bottom;
-      }
-    }
-
-    .side-menu-bottom {
-      cursor: pointer;
-      width: 100px;
-      text-align: center;
-      line-height: 3em;
-      .iconfont {
-        margin-right: 10px;
-      }
-
-      &.active {
-        background-color: #fff;
-        color: #314659;
-      }
     }
   }
-
-
 </style>
