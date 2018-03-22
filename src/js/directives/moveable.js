d9277a0acae85153cb1515df9c21eab3efa70756
蔡荣庚 <89987112@qq.com>
2018-03-22 14:46:29 +0800

    update

--- src/js/directives/moveable.js
+++ src/js/directives/moveable.js
@@ -1,4 +1,10 @@
 export default {
+  unbind(el) {
+    el.removeEventListener('mousedown', el.mousedown)
+    document.removeEventListener('mousemove', el.mousemove)
+    document.removeEventListener('mouseup', el.mouseup)
+    el.removeEventListener('mouseup', el.mouseup)
+  },
   inserted(el, binding, vNode, oldVNode) {
     // .base-component{
     //     position: relative;
@@ -17,33 +23,30 @@ export default {
     let
       startX = 0,
       startY = 0,
-      isMove = false,
-      resetPosition = ()=>{
+      isMove = false;
+
+    el.mousedown = (e) => {
+      el.style.transition = `none`
+      startX = e.clientX;
+      startY = e.clientY;
+      isMove = true;
+    },
+      el.mousemove = (e) => {
+        if (isMove) {
+          el.style.transform = `translate3d(${e.clientX - startX}px,${e.clientY - startY}px,0)`;
+          vNode.context.$emit('move', e);
+        }
+      },
+      el.mouseup = (e) => {
         isMove = false;
         startX = 0;
         startY = 0;
         el.style.transition = `transform .3s`
         el.style.transform = `translate3d(0,0,0)`;
       };
-
-
-    el.addEventListener('mousedown', (e) => {
-      el.style.transition = `none`
-      startX = e.clientX;
-      startY = e.clientY;
-      isMove = true;
-    })
-    document.addEventListener('mousemove', (e) => {
-      if (isMove) {
-        el.style.transform = `translate3d(${e.clientX - startX}px,${e.clientY - startY}px,0)`;
-        vNode.context.$emit('move',e);
-      }
-    })
-    document.addEventListener('mouseup', (e) => {
-      resetPosition();
-    })
-    el.addEventListener('mouseup', (e) => {
-      resetPosition();
-    })
+    el.addEventListener('mousedown', el.mousedown)
+    document.addEventListener('mousemove', el.mousemove)
+    document.addEventListener('mouseup', el.mouseup)
+    el.addEventListener('mouseup', el.mouseup)
   }
 }
