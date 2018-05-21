39b864703b71da04558546540254b6abc1472149
蔡荣庚 <89987112@qq.com>
2018-05-21 19:29:55 +0800

    update

--- webpack/src/js/plugins/element-extra.js
+++ webpack/src/js/plugins/element-extra.js
@@ -6,10 +6,50 @@ export default {
       });
     };
 
+    Vue.prototype.$formCheckContext = function(params,context){
+          return params.find(item => {
+              const
+                  name = item[0],
+                  //通过时的条件
+                  test = item[1],
+                  error = item[2];
+              if (test instanceof Function) {
+                  if (!test(context[name])) {
+                      this.$error(error);
+                      //中断循环
+                      return true;
+                  }
+              } else if (test instanceof RegExp) {
+                  if (!test.test(context[name])) {
+                      this.$error(error);
+                      //中断循环
+                      return true;
+                  }
+              } else {
+                  throw new Error('不支持的语法')
+              }
+          })
+      }
+
+        //form表单相关
+        //
+        // 多条件判断可以写多个
+        //  ['nameTitle',v=>v,"请指定姓名"],
+        //  ['nameTitle',v=>v.length<20,"姓名不能大于20个字符"],
+        //  
+        // if(this.$formCheck(
+        //   ['content',v=>v,"请输入内容！"]
+        // )){
+        //   return ;
+        // }
+      Vue.prototype.$formCheck = function(...params){
+          return this.$formCheckContext(params,this);
+      }
 
     Vue.prototype.$sccuess = function (message) {
-      this.$notify.sccuess({
-        message
+      this.$notify({
+        message,
+        type: 'success'
       });
     };
 
@@ -38,6 +78,7 @@ export default {
       };
       __loadings__.add(target);
       this.__is_loading__ = __loadings__;
+
       return handle;
     }
 
