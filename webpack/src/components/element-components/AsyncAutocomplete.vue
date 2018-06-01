47a69f2377232eee7ca05ef5f7c2545547e384b9
蔡荣庚 <89987112@qq.com>
2018-06-01 16:53:42 +0800

    update

---
+++ webpack/src/components/element-components/AsyncAutocomplete.vue
@@ -0,0 +1,30 @@
+<style lang="scss" scoped>
+
+</style>
+
+<template>
+  <el-autocomplete size="mini" v-model="realInputValue" :fetch-suggestions="querySearchAsync" placeholder="请输入内容" @select="handleSelect"></el-autocomplete>
+</template>
+
+<script>
+  export default {
+    props:['value','onRequest'],
+    data(){
+      let value = this.value||{};
+      return {
+        realInputValue:value.value
+      }
+    },
+    methods:{
+      querySearchAsync(queryString, cb){
+        // 数据格式 ,带value的数组
+        // [{ "value": "浏阳蒸菜", "address": "天山西路430号" }]
+        return this.onRequest(queryString).then(cb)
+      },
+      handleSelect(item){
+          this.$emit('input',item);
+      }
+    }
+  }
+</script>
+
